f2fbfb8949e14e8a1b3e6f48a75f9023532203b4
iszero12 <binj1246@gmail.com>
2024-09-02 19:17:43 +0900

    feat::Add Signup

--- app/src/main/java/com/doyoung/vote/SignupActivity.kt
+++ app/src/main/java/com/doyoung/vote/SignupActivity.kt
@@ -1,10 +1,17 @@
 package com.doyoung.vote
 
+import Join
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,28 @@ class SignupActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val usernameInput : EditText = findViewById(R.id.id_input)
+        val passwordInput : EditText = findViewById(R.id.password_input)
+        val passwordReInput : EditText = findViewById(R.id.password_re_input)
+        val SignupButton : Button = findViewById(R.id.signup_button)
+        val goLoginButton : Button = findViewById(R.id.go_login)
+        SignupButton.setOnClickListener(){
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            val passwordRe = passwordReInput.text.toString()
+            GlobalScope.launch {
+                val result = Join(username, password, passwordRe)
+                Log.d("result", result.toString())
+                if (result == "ok"){
+                    val intent = Intent(this@SignupActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            goLoginButton.setOnClickListener(){
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
